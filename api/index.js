b2a4db38345821974f60303fdfca45fd15d69f6c
hemant <hemvish11@gmail.com>
2024-03-09 20:59:43 +0530

    connected to mongo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000,()=>{
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to Mongo");
+    })
+    .catch((err) => {
+        console.log("Error connecting to Mongo:", err);
+    })
+
+app.listen(3000, () => {
     console.log("Server chalu h... jao dhoom machao...")
 })
